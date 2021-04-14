f4788cde1173aff047fdc61fca052b93ac73a85e
Justin Ngai <jn537@cornell.edu>
2021-04-14 15:11:30 -0400

    Reorder folders for modularity

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as Board } from "./Board";
-export { default as Sponsers } from "./Sponsers";
-export { default as Resources } from "./Resources";
+export { default as Navigation } from "./Navbar/Navigation";
+export { default as Footer } from "./Footer/Footer";
+export { default as Home } from "./Home/Home";
+export { default as Board } from "./Board/Board";
+export { default as Sponsers } from "./Sponser/Sponsers";
+export { default as Resources } from "./Resource/Resources";
