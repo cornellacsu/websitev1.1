ce7fc0682a242ecf873415ebb03cf3482fe38a11
Justin Ngai <jn537@cornell.edu>
2021-05-03 12:20:00 -0400

    Comment out old code

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "./Home.css";
-import typing from "./type.png";
+import "./Home.css";
+// import typing from "./type.png";
 
 function Home() {
     return (
--- src/components/Navbar/Navigation.jsx
+++ src/components/Navbar/Navigation.jsx
@@ -5,10 +5,10 @@ import { useState, useEffect, useRef } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 
-import { ReactComponent as BellIcon } from "./icons/bell.svg";
-import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
-import { ReactComponent as CaretIcon } from "./icons/caret.svg";
-import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+// import { ReactComponent as BellIcon } from "./icons/bell.svg";
+// import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+// import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+// import { ReactComponent as PlusIcon } from "./icons/plus.svg";
 import { ReactComponent as CogIcon } from "./icons/cog.svg";
 import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
 import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
