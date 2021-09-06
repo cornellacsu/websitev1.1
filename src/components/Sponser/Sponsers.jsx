89c9b23171a9fbffec26ca3b04ba4dc6d4aa4b8d
sohwijung <56904620+sohwijung@users.noreply.github.com>
2021-09-06 12:40:08 -0400

    Updated sponsors

--- src/components/Sponser/Sponsers.jsx
+++ src/components/Sponser/Sponsers.jsx
@@ -1,26 +1,80 @@
 import React from "react";
+import "./Sponsors.css";
+import deshaw from "./icons/deshaw.jpeg";
+import greenhills from "./icons/greenhills.jpeg";
+import capitalone from "./icons/capitalone.png";
+import amazonrobotics from "./icons/amazonrobotics.png";
+import bloomberg from "./icons/bloomberg.png";
+import stripe from "./icons/stripe.png";
 
 function Sponsers() {
     return (
         <div className="sponsers">
             <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">Sponser</h1>
-                        <p>
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                            Sponsers. Sponsers. Sponsers. Sponsers. Sponsers.
-                        </p>
+                <div class="row align-items-center">
+                    <div>
+                        <h1 class="font-weight-light">Sponsors</h1>
+                        <h5>Thank you to all our 2021 sponsors!</h5>
+                        <div class="tier">
+                            <h2 class="gold">Gold</h2>
+                            <a href="https://www.bloomberg.com/">
+                                <img
+                                    src={bloomberg}
+                                    alt="Bloomberg logo"
+                                    width="30%"
+                                    className="d-inline-block align-text-top">
+                                </img>
+                            </a>
+                            <a href="https://www.ghs.com/">
+                                <img
+                                        src={greenhills}
+                                        alt="Greenhills logo"
+                                        width="28%"
+                                        className="d-inline-block align-text-top">
+                                </img>
+                            </a>
+                        </div>
+
+                        <div class="tier">
+                            <h3 class="silver">Silver</h3>
+                            <a href="https://www.deshaw.com/">
+                                <img
+                                    src={deshaw}
+                                    alt="De Shaw logo"
+                                    width="22%"
+                                    className="d-inline-block align-text-top">
+                                </img>
+                            </a>
+                            {/* missing seven eight capital */}
+                            <a href="https://stripe.com/">
+                                <img
+                                    src={stripe}
+                                    alt="Stripe logo"
+                                    width="22%"
+                                    className="d-inline-block align-text-top">
+                                </img>
+                            </a>
+                        </div>
+
+                        <div class="tier">
+                            <h4 class="bronze">Bronze</h4>
+                            <a href="https://www.amazon.com/">
+                                <img
+                                    src={amazonrobotics}
+                                    alt="Amazon Robotics logo"
+                                    width="14%"
+                                    className="d-inline-block align-text-top">
+                                </img>
+                            </a>
+                            <a href="https://www.capitalone.com/">
+                                <img
+                                        src={capitalone}
+                                        alt="Capital One logo"
+                                        width="14%"
+                                        className="d-inline-block align-text-top">
+                                </img>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/Sponser/Tier.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// function Sponsers() {
+//     return (
+//         <div>
+//             <h2>
+//                 {Tier}
+//             </h2>
+//         </div>
+//     );
+// }
+
+// export default Tier;
