5fb9fe225bdd24f272c3b17c69bcc59d9a858afc
Justin Ngai <jn537@cornell.edu>
2021-10-06 17:09:04 -0400

    Revert "Update style throughout website"
    This reverts commit c43308892e569b8e0e97284e32b1482f727ba537.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,14 +7,14 @@ import gates from "./images/gates.svg";
 function Home() {
     return (
         <div className="main">
-            <div className="cover-logo-div">
+            <div className="section">
                 <img className="cover-logo" src={gates} alt="Gates Hall" />
             </div>
-            {/* <hr></hr> */}
+            <hr></hr>
             <div className="section">
                 <h1>About Us</h1>
                 <div className="subsection">
-                    <h2 className="subheader">Our Purpose</h2>
+                    <h2>Our Purpose</h2>
                     <p>
                         The Association of Computer Science Undergraduates
                         (ACSU) promotes educational, professional, and social
@@ -33,11 +33,11 @@ function Home() {
                     </p>
                 </div>
                 <div className="subsection">
-                    <h2 className="subheader">What We Do</h2>
+                    <h2>What We Do</h2>
                     <Roles />
                 </div>
             </div>
-            {/* <hr></hr> */}
+            <hr></hr>
             <div className="section">
                 <h1>Featured Articles</h1>
                 <Events />
--- src/components/Sponser/Sponsers.jsx
+++ src/components/Sponser/Sponsers.jsx
@@ -10,7 +10,7 @@ import stripe from "./icons/stripe.png";
 function Sponsers() {
     return (
         <div className="sponsers">
-            <div class="sponsers-container">
+            <div class="container">
                 <div class="row align-items-center">
                     <div>
                         <h1 class="font-weight-light">Sponsors</h1>
@@ -22,14 +22,16 @@ function Sponsers() {
                                     src={bloomberg}
                                     alt="Bloomberg logo"
                                     width="30%"
-                                    className="d-inline-block align-text-top"></img>
+                                    className="d-inline-block align-text-top">
+                                </img>
                             </a>
                             <a href="https://www.ghs.com/">
                                 <img
-                                    src={greenhills}
-                                    alt="Greenhills logo"
-                                    width="28%"
-                                    className="d-inline-block align-text-top"></img>
+                                        src={greenhills}
+                                        alt="Greenhills logo"
+                                        width="28%"
+                                        className="d-inline-block align-text-top">
+                                </img>
                             </a>
                         </div>
 
@@ -40,7 +42,8 @@ function Sponsers() {
                                     src={deshaw}
                                     alt="De Shaw logo"
                                     width="22%"
-                                    className="d-inline-block align-text-top"></img>
+                                    className="d-inline-block align-text-top">
+                                </img>
                             </a>
                             {/* missing seven eight capital */}
                             <a href="https://stripe.com/">
@@ -48,7 +51,8 @@ function Sponsers() {
                                     src={stripe}
                                     alt="Stripe logo"
                                     width="22%"
-                                    className="d-inline-block align-text-top"></img>
+                                    className="d-inline-block align-text-top">
+                                </img>
                             </a>
                         </div>
 
@@ -59,14 +63,16 @@ function Sponsers() {
                                     src={amazonrobotics}
                                     alt="Amazon Robotics logo"
                                     width="14%"
-                                    className="d-inline-block align-text-top"></img>
+                                    className="d-inline-block align-text-top">
+                                </img>
                             </a>
                             <a href="https://www.capitalone.com/">
                                 <img
-                                    src={capitalone}
-                                    alt="Capital One logo"
-                                    width="14%"
-                                    className="d-inline-block align-text-top"></img>
+                                        src={capitalone}
+                                        alt="Capital One logo"
+                                        width="14%"
+                                        className="d-inline-block align-text-top">
+                                </img>
                             </a>
                         </div>
                     </div>
