363db179d00586a60ec3109224c8a2640f96db9b
Justin Ngai <jn537@cornell.edu>
2021-10-27 13:06:15 -0400

    Fix spacing issue with sponsors page

--- src/components/Sponser/Sponsers.jsx
+++ src/components/Sponser/Sponsers.jsx
@@ -12,88 +12,78 @@ import blackstone from "./icons/blackstone.png";
 function Sponsers() {
     return (
         <div className="sponsers">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div>
-                        <h1 class="font-weight-light">Sponsors</h1>
-                        <h5>Thank you to all our 2021 sponsors!</h5>
-                        <div class="tier">
-                            <h2 class="gold">Gold</h2>
-                            <a href="https://www.bloomberg.com/">
-                                <img
-                                    src={bloomberg}
-                                    alt="Bloomberg logo"
-                                    width="30%"
-                                    className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                            <a href="https://www.ghs.com/">
-                                <img
-                                        src={greenhills}
-                                        alt="Greenhills logo"
-                                        width="28%"
-                                        className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                        </div>
+            <div class="row align-items-center">
+                <div>
+                    <h1 class="font-weight-light">Sponsors</h1>
+                    <h5>Thank you to all our 2021 sponsors!</h5>
+                    <div class="tier">
+                        <h2 class="gold">Gold</h2>
+                        <a href="https://www.bloomberg.com/">
+                            <img
+                                src={bloomberg}
+                                alt="Bloomberg logo"
+                                width="30%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
+                        <a href="https://www.ghs.com/">
+                            <img
+                                src={greenhills}
+                                alt="Greenhills logo"
+                                width="28%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
+                    </div>
 
-                        <div class="tier">
-                            <h3 class="silver">Silver</h3>
-                            <a href="https://www.deshaw.com/">
-                                <img
-                                    src={deshaw}
-                                    alt="De Shaw logo"
-                                    width="22%"
-                                    className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                            {/* missing seven eight capital */}
-                            <a href="https://stripe.com/">
-                                <img
-                                    src={stripe}
-                                    alt="Stripe logo"
-                                    width="22%"
-                                    className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                            <a href="https://facebook.com/">
-                                <img
-                                    src={facebook}
-                                    alt="Facebook logo"
-                                    width="24%"
-                                    className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                        </div>
+                    <div class="tier">
+                        <h3 class="silver">Silver</h3>
+                        <a href="https://www.deshaw.com/">
+                            <img
+                                src={deshaw}
+                                alt="De Shaw logo"
+                                width="22%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
+                        {/* missing seven eight capital */}
+                        <a href="https://stripe.com/">
+                            <img
+                                src={stripe}
+                                alt="Stripe logo"
+                                width="22%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
+                        <a href="https://facebook.com/">
+                            <img
+                                src={facebook}
+                                alt="Facebook logo"
+                                width="24%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
+                    </div>
 
-                        <div class="tier">
-                            <h4 class="bronze">Bronze</h4>
-                            <a href="https://www.amazon.com/">
-                                <img
-                                    src={amazonrobotics}
-                                    alt="Amazon Robotics logo"
-                                    width="14%"
-                                    className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                            <a href="https://www.capitalone.com/">
-                                <img
-                                        src={capitalone}
-                                        alt="Capital One logo"
-                                        width="14%"
-                                        className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                            {/*how do I fix the spacing and alignment for this one*/}
-                            <a href="https://www.blackstone.com/">
-                                <img
-                                        src={blackstone}
-                                        alt="Blackstone logo"
-                                        width="14%"
-                                        className="d-inline-block align-text-top">
-                                </img>
-                            </a>
-                        </div>
+                    <div class="tier">
+                        <h4 class="bronze">Bronze</h4>
+                        <a href="https://www.amazon.com/">
+                            <img
+                                src={amazonrobotics}
+                                alt="Amazon Robotics logo"
+                                width="14%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
+                        <a href="https://www.capitalone.com/">
+                            <img
+                                src={capitalone}
+                                alt="Capital One logo"
+                                width="14%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
+                        {/*how do I fix the spacing and alignment for this one*/}
+                        <a href="https://www.blackstone.com/">
+                            <img
+                                src={blackstone}
+                                alt="Blackstone logo"
+                                width="14%"
+                                className="d-inline-block align-text-top"></img>
+                        </a>
                     </div>
                 </div>
             </div>
