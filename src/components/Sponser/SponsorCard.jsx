c616ed208c866415c3703dcf7ae5b9213255fca3
Mahi Nair <mahifreya@gmail.com>
2021-11-06 19:33:33 -0400

    changing card for blackstone logo

--- src/components/Sponser/SponsorCard.jsx
+++ src/components/Sponser/SponsorCard.jsx
@@ -2,8 +2,24 @@ import React from "react";
 import {Palette} from 'react-palette';
 import "./Sponsors.css";
 
-function SponsorCard({ link, img, imgName, width}) {
-  return (
+function SponsorCard({ link, img, imgName}) {
+  return ( imgName === "blackstone" ?
+  <Palette src={img}>
+  {({ data, loading, error }) => (
+    <div class="color-container-sponsors" style={{ 'background-color': data.vibrant }}>
+      {/* what does this actually do? How do I make the box around blackstone not white.. */}
+        <div id = "blackstone" class="white-container-sponsors">
+        <a href={link}>
+            <img
+                src={img}
+                alt={imgName}
+                className="d-inline-block align-text-top">
+            </img>
+        </a>
+        </div>
+    </div>
+  )}
+</Palette> :
 
 <Palette src={img}>
 {({ data, loading, error }) => (
