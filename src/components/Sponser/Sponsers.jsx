d842ec2c7fccdb05944ec2104a65eb2afb283046
Justin Ngai <jn537@cornell.edu>
2021-11-06 20:06:32 -0400

    autoformat

--- src/components/Sponser/Sponsers.jsx
+++ src/components/Sponser/Sponsers.jsx
@@ -9,12 +9,10 @@ import stripe from "./icons/stripe.png";
 import facebook from "./icons/facebook.png";
 import blackstone from "./icons/blackstone.png";
 import SponsorCard from "./SponsorCard";
-import { useState} from "react";
+import { useState } from "react";
 import { DriveEtaSharp } from "@material-ui/icons";
 
-
 function Sponsors() {
-
     //manually add to just this list, the code does everything else for you.
     const sponsors = [
         {
@@ -43,7 +41,7 @@ function Sponsors() {
             link: "https://stripe.com/",
             img: stripe,
             imgName: "Stripe logo",
-            width:"22%",
+            width: "22%",
         },
         {
             class: "silver",
@@ -72,12 +70,12 @@ function Sponsors() {
             img: blackstone,
             imgName: "Blackstone logo",
             width: "14%",
-        }
-    ]
+        },
+    ];
 
-    const getGold = sponsors.filter((sponsor)=> (sponsor.class == "gold"));
-    const getSilver = sponsors.filter((sponsor)=> (sponsor.class == "silver"));
-    const getBronze = sponsors.filter((sponsor)=> (sponsor.class == "bronze"));
+    const getGold = sponsors.filter((sponsor) => sponsor.class == "gold");
+    const getSilver = sponsors.filter((sponsor) => sponsor.class == "silver");
+    const getBronze = sponsors.filter((sponsor) => sponsor.class == "bronze");
 
     const [gold, setGold] = useState(getGold);
     const [silver, setSilver] = useState(getSilver);
@@ -91,59 +89,59 @@ function Sponsors() {
                     <h5>Thank you to all our 2021 sponsors!</h5>
                     {/* I wish there is a way to combine all three of these into one and use some sort of filtering to vertically 
                     place them on the page/in a container? */}
-                  <div class="sponsors-tier tier">
+                    <div class="sponsors-tier tier">
                         <h2 class="gold">Gold</h2>
-                            <div class="tier-container tier-gold">
-                        {gold.map((sponsor) => {
-                            return (
-                                <SponsorCard
-                                    itemId={sponsor.imgName} // NOTE: itemId is required for track items
-                                    link = {sponsor.link}
-                                    img = {sponsor.img}
-                                    imgName = {sponsor.imgName}
-                                    width = {sponsor.width}
-                                />
-                            );
-                        })}
+                        <div class="tier-container tier-gold">
+                            {gold.map((sponsor) => {
+                                return (
+                                    <SponsorCard
+                                        itemId={sponsor.imgName} // NOTE: itemId is required for track items
+                                        link={sponsor.link}
+                                        img={sponsor.img}
+                                        imgName={sponsor.imgName}
+                                        width={sponsor.width}
+                                    />
+                                );
+                            })}
                         </div>
                     </div>
-                
+
                     <div class="tier">
                         <h3 class="silver">Silver</h3>
                         <div class="tier-container tier-silver">
-                        {silver.map((sponsor) => {
-                            return (
-                                <SponsorCard
-                                    itemId={sponsor.imgName} // NOTE: itemId is required for track items
-                                    link = {sponsor.link}
-                                    img = {sponsor.img}
-                                    imgName = {sponsor.imgName}
-                                    width = {sponsor.width}
-                                />
-                            );
-                        })}
+                            {silver.map((sponsor) => {
+                                return (
+                                    <SponsorCard
+                                        itemId={sponsor.imgName} // NOTE: itemId is required for track items
+                                        link={sponsor.link}
+                                        img={sponsor.img}
+                                        imgName={sponsor.imgName}
+                                        width={sponsor.width}
+                                    />
+                                );
+                            })}
                         </div>
                         {/* missing seven eight capital*/}
                     </div>
                     <div class="tier">
                         <h4 class="bronze">Bronze</h4>
                         <div class="tier-container tier-bronze">
-                        {bronze.map((sponsor) => {
-                            return (
-                                <SponsorCard
-                                    itemId={sponsor.imgName} // NOTE: itemId is required for track items
-                                    link = {sponsor.link}
-                                    img = {sponsor.img}
-                                    imgName = {sponsor.imgName}
-                                    width = {sponsor.width}
-                                />
-                            );
-                        })}
+                            {bronze.map((sponsor) => {
+                                return (
+                                    <SponsorCard
+                                        itemId={sponsor.imgName} // NOTE: itemId is required for track items
+                                        link={sponsor.link}
+                                        img={sponsor.img}
+                                        imgName={sponsor.imgName}
+                                        width={sponsor.width}
+                                    />
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 }
 
