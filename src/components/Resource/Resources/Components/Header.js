2d6781c555ee320b45566ad1f992624aecede48b
Justin Ngai <jn537@cornell.edu>
2022-01-03 18:34:41 -0800

    Auto format

--- src/components/Resource/Resources/Components/Header.js
+++ src/components/Resource/Resources/Components/Header.js
@@ -1,48 +1,48 @@
-import React, {Component, Fragment} from "react";
-import Foreword from "../Pages/Foreword"
-import FAQ from "../Pages/FAQ"
-import Courses from "../Pages/Courses"
-import Clubs from "../Pages/Clubs"
-import Research from "../Pages/Research"
-import Graduate from "../Pages/Graduate"
-import Anecdotes from "../Pages/Anecdotes"
+import React, { Component, Fragment } from "react";
+import Foreword from "../Pages/Foreword";
+import FAQ from "../Pages/FAQ";
+import Courses from "../Pages/Courses";
+import Clubs from "../Pages/Clubs";
+import Research from "../Pages/Research";
+import Graduate from "../Pages/Graduate";
+import Anecdotes from "../Pages/Anecdotes";
 
-class Header extends Component{
-    constructor(props){
+class Header extends Component {
+    constructor(props) {
         super(props);
     }
-    
-    render(){
-    let heading = null;
-    const current = this.props.current;
-    switch(current){
-        case 0:
-            heading = "Foreword";
-            break;
-        case 1:
-            heading = "FAQ";
-            break;
-        case 2:
-            heading = "Courses";
-            break;
-        case 3:
-            heading = "Clubs";
-            break;
-        case 4:
-            heading = "Research";
-            break;
-        case 5:
-            heading = "Graduate";
-            break;
-        case 6:
-            heading = "Anecdotes";
-            break;
-    }
-    return(
-        <div class="Header">
-            <h1>{heading}</h1>
-        </div>
-    )
+
+    render() {
+        let heading = null;
+        const current = this.props.current;
+        switch (current) {
+            case 0:
+                heading = "Foreword";
+                break;
+            case 1:
+                heading = "FAQ";
+                break;
+            case 2:
+                heading = "Courses";
+                break;
+            case 3:
+                heading = "Clubs";
+                break;
+            case 4:
+                heading = "Research";
+                break;
+            case 5:
+                heading = "Graduate";
+                break;
+            case 6:
+                heading = "Anecdotes";
+                break;
+        }
+        return (
+            <div class="Header">
+                <h1>{heading}</h1>
+            </div>
+        );
     }
 }
 
