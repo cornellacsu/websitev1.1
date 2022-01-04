fe16fd88fe5bd8c6bbc5fa0beda8297e7d93398e
Justin Ngai <jn537@cornell.edu>
2022-01-03 18:36:18 -0800

    Auto format

--- src/components/Resource/Resources/Components/SideBar.js
+++ src/components/Resource/Resources/Components/SideBar.js
@@ -1,24 +1,52 @@
-import React, {Component, Fragment} from "react";
+import React, { Component, Fragment } from "react";
 
-class SideBar extends Component{
-    constructor(props){
+class SideBar extends Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <router>
                 <div className="SideBar">
-                    <a style={{color:this.props.checkSection(0)}} onClick={() => this.props.changeSection(0)}>Foreword</a>
-                    <a style={{color:this.props.checkSection(1)}} onClick={() => this.props.changeSection(1)}>FAQ</a>
-                    <a style={{color:this.props.checkSection(2)}} onClick={() => this.props.changeSection(2)}>Courses</a>
-                    <a style={{color:this.props.checkSection(3)}} onClick={() => this.props.changeSection(3)}>Clubs</a>
-                    <a style={{color:this.props.checkSection(4)}} onClick={() => this.props.changeSection(4)}>Research</a>
-                    <a style={{color:this.props.checkSection(5)}} onClick={() => this.props.changeSection(5)}>Graduate</a>
-                    <a style={{color:this.props.checkSection(6)}} onClick={() => this.props.changeSection(6)}>Anecdotes</a>
+                    <a
+                        style={{ color: this.props.checkSection(0) }}
+                        onClick={() => this.props.changeSection(0)}>
+                        Foreword
+                    </a>
+                    <a
+                        style={{ color: this.props.checkSection(1) }}
+                        onClick={() => this.props.changeSection(1)}>
+                        FAQ
+                    </a>
+                    <a
+                        style={{ color: this.props.checkSection(2) }}
+                        onClick={() => this.props.changeSection(2)}>
+                        Courses
+                    </a>
+                    <a
+                        style={{ color: this.props.checkSection(3) }}
+                        onClick={() => this.props.changeSection(3)}>
+                        Clubs
+                    </a>
+                    <a
+                        style={{ color: this.props.checkSection(4) }}
+                        onClick={() => this.props.changeSection(4)}>
+                        Research
+                    </a>
+                    <a
+                        style={{ color: this.props.checkSection(5) }}
+                        onClick={() => this.props.changeSection(5)}>
+                        Graduate
+                    </a>
+                    <a
+                        style={{ color: this.props.checkSection(6) }}
+                        onClick={() => this.props.changeSection(6)}>
+                        Anecdotes
+                    </a>
                 </div>
             </router>
-        )
+        );
     }
 }
 
