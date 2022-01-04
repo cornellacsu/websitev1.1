07bb4a4afff87cb9b850b7f7218d411aed49d394
Justin Ngai <jn537@cornell.edu>
2022-01-03 18:38:42 -0800

    Remove dead code

--- src/components/Resource/Resources/Components/Section.js
+++ src/components/Resource/Resources/Components/Section.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import Foreword from "../Pages/Foreword";
 import FAQ from "../Pages/FAQ";
 import Courses from "../Pages/Courses";
@@ -8,42 +8,32 @@ import Graduate from "../Pages/Graduate";
 import Anecdotes from "../Pages/Anecdotes";
 
 class Section extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
-        let heading = null;
         let section = null;
         const current = this.props.current;
         switch (current) {
             case 0:
-                heading = "Foreword";
                 section = <Foreword />;
                 break;
             case 1:
-                heading = "FAQ";
                 section = <FAQ />;
                 break;
             case 2:
-                heading = "Courses";
                 section = <Courses />;
                 break;
             case 3:
-                heading = "Clubs";
                 section = <Clubs />;
                 break;
             case 4:
-                heading = "Research";
                 section = <Research />;
                 break;
             case 5:
-                heading = "Graduate";
                 section = <Graduate />;
                 break;
             case 6:
-                heading = "Anecdotes";
                 section = <Anecdotes />;
+                break;
+            default:
                 break;
         }
         return (
