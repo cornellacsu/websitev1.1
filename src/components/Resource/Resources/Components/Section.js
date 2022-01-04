1ab00321d34ae2027157ae379a8826d99e5f2159
Justin Ngai <jn537@cornell.edu>
2022-01-03 18:36:51 -0800

    Auto format

--- src/components/Resource/Resources/Components/Section.js
+++ src/components/Resource/Resources/Components/Section.js
@@ -1,56 +1,56 @@
-import React, {Component, Fragment} from "react";
-import Foreword from "../Pages/Foreword"
-import FAQ from "../Pages/FAQ"
-import Courses from "../Pages/Courses"
-import Clubs from "../Pages/Clubs"
-import Research from "../Pages/Research"
-import Graduate from "../Pages/Graduate"
-import Anecdotes from "../Pages/Anecdotes"
+import React, { Component, Fragment } from "react";
+import Foreword from "../Pages/Foreword";
+import FAQ from "../Pages/FAQ";
+import Courses from "../Pages/Courses";
+import Clubs from "../Pages/Clubs";
+import Research from "../Pages/Research";
+import Graduate from "../Pages/Graduate";
+import Anecdotes from "../Pages/Anecdotes";
 
-class Section extends Component{
-    constructor(props){
+class Section extends Component {
+    constructor(props) {
         super(props);
     }
-    
-    render(){
-    let heading = null;
-    let section = null;
-    const current = this.props.current;
-    switch(current){
-        case 0:
-            heading = "Foreword";
-            section = <Foreword />
-            break;
-        case 1:
-            heading = "FAQ";
-            section = <FAQ />
-            break;
-        case 2:
-            heading = "Courses";
-            section = <Courses />
-            break;
-        case 3:
-            heading = "Clubs";
-            section = <Clubs />
-            break;
-        case 4:
-            heading = "Research";
-            section = <Research />
-            break;
-        case 5:
-            heading = "Graduate";
-            section = <Graduate />
-            break;
-        case 6:
-            heading = "Anecdotes";
-            section = <Anecdotes />
-            break;
-    }
-    return(
-        <div class="Section">
-            <div>{section}</div>
-        </div>
-    )
+
+    render() {
+        let heading = null;
+        let section = null;
+        const current = this.props.current;
+        switch (current) {
+            case 0:
+                heading = "Foreword";
+                section = <Foreword />;
+                break;
+            case 1:
+                heading = "FAQ";
+                section = <FAQ />;
+                break;
+            case 2:
+                heading = "Courses";
+                section = <Courses />;
+                break;
+            case 3:
+                heading = "Clubs";
+                section = <Clubs />;
+                break;
+            case 4:
+                heading = "Research";
+                section = <Research />;
+                break;
+            case 5:
+                heading = "Graduate";
+                section = <Graduate />;
+                break;
+            case 6:
+                heading = "Anecdotes";
+                section = <Anecdotes />;
+                break;
+        }
+        return (
+            <div class="Section">
+                <div>{section}</div>
+            </div>
+        );
     }
 }
 
