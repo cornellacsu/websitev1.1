32ade4a66acbdc3c5776d963cc85fc4d495b4b73
Kenneth Chiem <kc696@cornell.edu>
2023-10-26 17:05:56 -0400

    Refactor code for react-router upgrade

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,38 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
-    Navigation,
-    Footer,
-    Home,
-    Board,
-    Sponsors,
-    Resources,
+  Navigation,
+  Footer,
+  Home,
+  Board,
+  Sponsors,
+  Resources,
 } from "./components";
 import ProgressBar from "react-scroll-progress-bar";
 function App() {
-    return (
-        <div className="App">
-            <ProgressBar bgcolor="#b81c34" />
-            <Router>
-                <Navigation />
-                <Switch>
-                    <Route path="/" exact component={() => <Home />} />
-                    <Route path="/board" exact component={() => <Board />} />
-                    <Route
-                        path="/sponsors"
-                        exact
-                        component={() => <Sponsors />}
-                    />
-                    <Route
-                        path="/resources"
-                        exact
-                        component={() => <Resources />}
-                    />
-                </Switch>
-                <Footer />
-            </Router>
-        </div>
-    );
+  return (
+    <div className="App">
+      <ProgressBar bgcolor="#b81c34" />
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/board" exact>
+            <Board />
+          </Route>
+          <Route path="/sponsors" exact>
+            <Sponsors />
+          </Route>
+          <Route path="/resources" exact>
+            <Resources />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
