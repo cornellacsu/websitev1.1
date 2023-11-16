965c0692529d5755f78b83d62295697f8a6b1e61
Daisy Chang <146611553+daisychangm@users.noreply.github.com>
2023-11-15 21:50:56 -0500

    Added access code login screen for sponsors

--- client/src/components/ResumeBook/GetStarted.jsx
+++ client/src/components/ResumeBook/GetStarted.jsx
@@ -11,6 +11,8 @@ import {useState} from "react";
 
 export default function GetStarted() {
     const [open, setOpen] = React.useState(false);
+    const [isSponsor, setIsSponsor] = React.useState(false)
+    const [isStudent, setIsStudent] = React.useState(false)
     const [formValues, setFormValues] = useState({
       email: "",
       name: "",
@@ -23,9 +25,23 @@ export default function GetStarted() {
         [id]: value,
       });
     };
+
+    const handleToggleSponsor = () => {
+        setIsSponsor(true)
+    };
+
+    const handleToggleStudent = () => {
+        setIsStudent(true)
+    };
+
+    const handleToggleHome = () => {
+        setIsSponsor(false)
+        setIsStudent(false)
+    }
   
     const handleClickOpen = () => {
       setOpen(true);
+      handleToggleHome();
     };
   
     const handleClose = () => {
@@ -79,23 +95,78 @@ export default function GetStarted() {
     })(Button);
   
     return (
-      <div>
-        <GetStartedButton onClick={handleClickOpen}>
-          Get Started
-        </GetStartedButton>
-        <Dialog open={open} onClose={handleClose}>
-          <form onSubmit={handleSubscribe}>
-            <DialogTitle style={{marginTop: '30px', color: '#000000'}}>I am a...</DialogTitle>
-            <DialogContent>
-              <Button onClick={handleClose} style={{fontWeight: 700, margin: '50px', color: '#b01c33', marginRight: '35px', marginTop: '35px'}}>
-                Student
-              </Button>
-              <Button onClick={handleClose} style={{fontWeight: 700, margin: '50px', color: '#b01c33', marginLeft: '35px', marginTop: '35px'}}>
-                Sponsor
-              </Button>
-            </DialogContent>
-          </form>
-        </Dialog>
+        <div>
+            <GetStartedButton onClick={handleClickOpen}>
+            Get Started
+            </GetStartedButton>
+            <Dialog 
+            open={open} 
+            onClose={handleClose} 
+            maxWidth='md'>
+                <DialogContent>
+                    {isStudent && (
+                        <div>
+                            I am a student!
+                        </div>
+                    )}
+                    {isSponsor && (
+                        <div style={{width: '400px'}}>
+                            <DialogTitle style={{color: '#000000'}}>Please enter your access code to view the Resume Book.</DialogTitle>
+                            <div>
+                            <TextField
+                                autoFocus
+                                required
+                                margin="dense"
+                                id="access-code"
+                                label="Access Code"
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                                onChange={handleInputChange}
+                                />
+                            <DialogContentText style={{marginBottom: '15px'}}>
+                                Don't have a code? <a href="mailto:acsu@cornell.edu">Email us.</a>
+                            </DialogContentText>
+                            </div>
+                            <DialogActions>
+                                <Button onClick={handleToggleHome} style={{fontWeight: 700}}>
+                                    Back
+                                </Button>
+                                <Button onClick={handleClose}
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit"
+                                    style={{fontWeight: 700}}>
+                                    Submit
+                                </Button>
+                            </DialogActions>
+                        </div>
+                    )}
+                    {!isStudent && !isSponsor && (
+                        <div style={{padding: '50px'}}>
+                            <DialogTitle style={{color: '#000000'}}>I am a...</DialogTitle>
+                            <div style={{marginTop: '30px'}}>
+                                <Button onClick={handleToggleStudent} style={{
+                                    fontWeight: 700,
+                                    color: '#b01c33',
+                                    marginRight: '40px',
+                                    fontSize: '1.2rem'
+                                    }}>
+                                    Student
+                                </Button>
+                                <Button onClick={handleToggleSponsor} style={{
+                                    fontWeight: 700,
+                                    color: '#b01c33', 
+                                    marginLeft: '40px',
+                                    fontSize: '1.2rem'
+                                    }}>
+                                    Sponsor
+                                </Button>
+                            </div>
+                        </div>
+                    )}
+                </DialogContent>
+            </Dialog>
       </div>
     );
-  }
+}
