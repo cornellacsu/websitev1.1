ba78dae9208c0cdd44e0e2b7af622383865f8545
Kenneth Chiem <kc696@cornell.edu>
2024-03-17 12:40:00 -0400

    add skeleton code for resumebook

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home, Board, Sponsors, Resources } from "./pages";
+import { Home, Board, Sponsors, Resources, Test, ResumeBook } from "./pages";
 import { Footer, Navigation } from "./molecules";
 import ProgressBar from "react-scroll-progress-bar";
 function App() {
@@ -9,12 +9,13 @@ function App() {
       <ProgressBar bgcolor="#b81c34" />
       <Router>
         <Navigation />
-
         <Switch>
           <Route path="/" exact component={() => <Home />} />
           <Route path="/board" exact component={() => <Board />} />
           <Route path="/sponsors" exact component={() => <Sponsors />} />
           <Route path="/resources" exact component={() => <Resources />} />
+          <Route path="/test" exact component={() => <Test />} />
+          <Route path="/resumebook" exact component={() => <ResumeBook />} />
         </Switch>
         <Footer />
       </Router>
---
+++ client/src/pages/ResumeBook/ResumeBook.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ResumeBook.css";
+
+function ResumeBook() {
+  return (
+    <div>
+      <h1>ResumeBook</h1>
+    </div>
+  );
+}
+
+export default ResumeBook;
