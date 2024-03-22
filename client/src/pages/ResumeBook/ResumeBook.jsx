98cbb2abfb911aa469cd383e266558866c0a9938
Kenneth Chiem <kc696@cornell.edu>
2024-03-22 16:33:47 -0400

    formatting changes

--- client/src/pages/ResumeBook/ResumeBook.jsx
+++ client/src/pages/ResumeBook/ResumeBook.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import "./ResumeBook.css";
-import gates_hall from "./gates_hall.png"
+import gates_hall from "./gates_hall.png";
 import TextField from "@mui/material/TextField";
-import LockIcon from '@mui/icons-material/Lock';
+import LockIcon from "@mui/icons-material/Lock";
 
 function ResumeBook() {
   return (
-    <div className="container">
-      <h3>Association of Computer Science Undergraduates</h3>
-      <h1>Resume Book Database</h1>
-      <LockIcon />
+    <>
       <img src={gates_hall} alt="Gates hall" className="image" />
-      <TextField id="outlined-basic" placeholder="Password" variant="standard" type="password" className="passwordfield" />
-      <h2>This page is only accessible to ACSU Sponsors.</h2>
-    </div>
+      <div className="img-container">
+        <h3>Association of Computer Science Undergraduates</h3>
+        <h1>Resume Book Database</h1>
+        <LockIcon />
+        {/* <img src={gates_hall} alt="Gates hall" className="image" /> */}
+        <TextField
+          id="outlined-basic"
+          placeholder="Password"
+          variant="standard"
+          type="password"
+          className="passwordfield"
+        />
+        <h2 style={{ marginTop: "8rem" }}>
+          This page is only accessible to ACSU Sponsors.
+        </h2>
+      </div>
+    </>
   );
 }
 
