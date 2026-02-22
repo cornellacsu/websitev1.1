f66ae07668c62949ec51c151c9a2a491105bc961
rubypen <ruby.pen23@gmail.com>
2026-02-22 13:20:35 -0500

    adapt navbar for dark mode

--- client/src/molecules/Navbar/Navigation.jsx
+++ client/src/molecules/Navbar/Navigation.jsx
@@ -1,36 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navigation.css";
 import { Link, withRouter } from "react-router-dom";
-import styled from "@emotion/styled";
 import logo from "./icons/logo.png";
 import { Fade } from "react-awesome-reveal";
-import JoinUs from "../../atoms/JoinUs";
 
-const MyNavLink = styled(Link)`
-  font-size: 20px;
-  font-weight: bolder;
-  color: black;
-  margin-left: 15px;
-  text-decoration: none;
-  display: inline-block;
-  position: relative;
-  ::after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: #b01c33;
-    transform-origin: bottom right;
-    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
-  }
-  :hover::after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
-  }
-`;
+const NAV_ITEMS = [
+  { label: "Board", to: "/board" },
+  { label: "Sponsors", to: "/sponsors" },
+  { label: "Resources", to: "/resources" },
+  { label: "Calendar", to: "/calendar" },
+  { label: "Events", to: "/events" },
+];
 
 const openInNewTab = (url) => {
   const newWindow = window.open(url, "_blank", "noopener,noreferrer");
@@ -38,50 +18,69 @@ const openInNewTab = (url) => {
 };
 
 function Navigation(props) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const interval = 100;
+
+  useEffect(() => {
+    setIsMobileMenuOpen(false);
+  }, [props.location.pathname]);
+
+  const handleJoinUsClick = () => {
+    openInNewTab("https://forms.gle/AyRTriREGNp2xj927");
+    setIsMobileMenuOpen(false);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <Link className="acsu-logo-wrapper" to="/">
+      <Link className="acsu-logo-wrapper" to="/" onClick={closeMobileMenu}>
         <Fade triggerOnce>
           <img src={logo} alt="ACSU logo" className="acsu-logo-img"></img>
         </Fade>
       </Link>
-      <div className="container">
-        <Fade direction="up" triggerOnce delay={interval}>
-          <MyNavLink className="nav-button" to="/board">
-            Board
-          </MyNavLink>
-        </Fade>
-        <Fade direction="up" triggerOnce delay={2 * interval}>
-          <MyNavLink className="nav-button" to="/sponsors">
-            Sponsors
-          </MyNavLink>
-        </Fade>
-        <Fade direction="up" triggerOnce delay={3 * interval}>
-          <MyNavLink className="nav-button" to="/resources">
-            Resources
-          </MyNavLink>
-        <Fade direction="up" triggerOnce delay={3 * interval}>
-          <MyNavLink className="nav-button" to="/calendar">
-            Calendar
-          </MyNavLink>
-        </Fade>  
-        </Fade>
-        <Fade direction="up" triggerOnce delay={3 * interval}>
-          <MyNavLink
-            className="nav-button"
-            onClick={() => openInNewTab("https://bit.ly/acsufall25")}
+      <button
+        type="button"
+        className={`hamburger-button ${isMobileMenuOpen ? "is-open" : ""}`}
+        aria-label="Toggle navigation menu"
+        aria-expanded={isMobileMenuOpen}
+        aria-controls="navbar-links"
+        onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div
+        id="navbar-links"
+        className={`nav-links ${isMobileMenuOpen ? "is-open" : ""}`}
+      >
+        {NAV_ITEMS.map((item, index) => (
+          <Fade
+            key={item.to}
+            direction="up"
+            triggerOnce
+            delay={(index + 1) * interval}
           >
-            Officer Applications
-          </MyNavLink>
-        </Fade>
-        <Fade direction="up" triggerOnce delay={4 * interval}>
-        <MyNavLink
-            className="nav-button"
-            onClick={() => openInNewTab("https://forms.gle/AyRTriREGNp2xj927")}
+            <Link className="nav-link" to={item.to} onClick={closeMobileMenu}>
+              {item.label}
+            </Link>
+          </Fade>
+        ))}
+        <Fade
+          direction="up"
+          triggerOnce
+          delay={(NAV_ITEMS.length + 1) * interval}
+        >
+          <button
+            type="button"
+            className="nav-link nav-link-button"
+            onClick={handleJoinUsClick}
           >
             Join Us!
-          </MyNavLink>
+          </button>
         </Fade>
       </div>
     </nav>
