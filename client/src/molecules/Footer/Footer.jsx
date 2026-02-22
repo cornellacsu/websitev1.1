44b99f962b7977f302fbe5f80af0f3f02a8b0089
rubypen <ruby.pen23@gmail.com>
2026-02-22 15:44:57 -0500

    adapt new footer design

--- client/src/molecules/Footer/Footer.jsx
+++ client/src/molecules/Footer/Footer.jsx
@@ -1,51 +1,56 @@
 import React from "react";
-import copyright from "./copyright.svg";
-import youtube from "./youtube.svg";
-import facebook from "./facebook.svg";
-import instagram from "./instagram.svg";
+import { Link } from "react-router-dom";
+import logo from "../Navbar/icons/logo.png";
 import "./Footer.css";
-import { Flex, Box } from "reflexbox";
 
 function Footer() {
   return (
-    <footer className="footer-section justify-center">
-      <div className="sm-handles">
-        <Flex flexWrap="wrap" mx={50}>
-          <Box width={[1 / 2]} p={1}>
-            <div className="icon">
-              <a href="https://www.facebook.com/CornellACSU">
-                <img src={facebook} alt={"fb"}></img>
-              </a>
-            </div>
-          </Box>
-          <Box width={[1 / 2]} p={1}>
-            <div className="icon">
-              <a href="https://www.instagram.com/cornellacsu/">
-                <img src={instagram} alt={"ig"}></img>
-              </a>
-            </div>
-          </Box>
-          {/* <Box width={[1 / 3]} p={2}>
-            <div className="icon">
-              <a href="https://www.youtube.com/channel/UCrL2JCxZi14q_nzuF04TlXQ">
-                <img src={youtube} alt={"yt"}></img>
-              </a>
-            </div>
-          </Box> */}
-        </Flex>
+    <footer className="footer-section">
+      <div className="footer-inner-stroke">
+        <div className="footer-bottom-left">
+          <img src={logo} alt="ACSU logo" className="footer-logo" />
+          <p className="footer-copyright">@ 2026 ACSU at Cornell</p>
+        </div>
+
+        <div className="footer-links">
+          <div className="footer-links-row footer-links-row-top">
+            <Link to="/board">Team</Link>
+            <span>&middot;</span>
+            <Link to="/resources">Resources</Link>
+            <span>&middot;</span>
+            <Link to="/sponsors">Sponsorship</Link>
+            <span>&middot;</span>
+            <Link to="/calendar">Events</Link>
+          </div>
+          <div className="footer-links-row footer-links-row-bottom">
+            <Link to="/resources">Campus Groups</Link>
+            <a
+              href="https://edstem.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ed Discussion
+            </a>
+            <a
+              href="https://www.instagram.com/cornellacsu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              IG: @cornellacsu
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="copyright">
-        <img src={copyright} alt={"copyright"}></img>
-        <p>
-          <a
-            href="https://hr.cornell.edu/about/workplace-rights/equal-education-and-employment"
-            style={{ color: "black", textDecoration: "underline" }}
-          >
-            Equal Education and Employment
-          </a>{" "}
-          | This organization is a registered student organization of Cornell
-          University
-        </p>
+      <div className="footer-legal-bar">
+        <a
+          href="https://hr.cornell.edu/about/workplace-rights/equal-education-and-employment"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Equal Education and Employment
+        </a>{" "}
+        | This organization is a registered student organization of Cornell
+        University
       </div>
     </footer>
   );
