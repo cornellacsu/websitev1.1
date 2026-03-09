a5f88ffd5068048a52cae06dc86f15c79516da15
Chelsea She <cms556@cornell.edu>
2026-03-09 15:13:55 -0400

    created an empty project and renamed root directory

---
+++ .next/dev/server/chunks/ssr/acsu_websitev2_0__next-internal_server_app_page_actions_e8102c81.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/acsu_websitev2.0/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=acsu_websitev2_0__next-internal_server_app_page_actions_e8102c81.js.map
---
+++ .next/dev/static/chunks/acsu_websitev2_0_app_layout_tsx_2db746dd._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__bfc5b1d7._.css"
+],
+    source: "dynamic"
+});
